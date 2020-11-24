8b959d2f0b5d51e027e25f849e9636fae159619a
mixxamm <maximjanssensj5@gmail.com>
2020-11-24 11:25:22 +0100

    Voeg test grid toe

--- TheFellowShipMobileApp/TheFellowShipMobileApp/Views/HomeView.xaml.cs
+++ TheFellowShipMobileApp/TheFellowShipMobileApp/Views/HomeView.xaml.cs
@@ -21,11 +21,36 @@ namespace TheFellowShipMobileApp
             StartLayout.IsVisible = false;
             GameLayout.IsVisible = true;
             IdLabel.Text = version;
+            CreateGameGrid();
         }
 
         protected override bool OnBackButtonPressed()
         {
             return true;
+        }
+
+        public void CreateGameGrid()
+        {
+            gameTiles.RowDefinitions.Add(new RowDefinition());
+            gameTiles.RowDefinitions.Add(new RowDefinition());
+            gameTiles.RowDefinitions.Add(new RowDefinition());
+            gameTiles.ColumnDefinitions.Add(new ColumnDefinition());
+            gameTiles.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 2; columnIndex++)
+                {
+
+                    var label = new Label
+                    {
+                        Text = $"${columnIndex} - {rowIndex}",
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Center
+                    };
+                    gameTiles.Children.Add(label, columnIndex, rowIndex);
+                }
+            }
         }
     }
 }
