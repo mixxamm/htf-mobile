f50ea5f293638d94cc66564b32795ff035d99e86
mixxamm <maximjanssensj5@gmail.com>
2020-11-24 13:45:59 +0100

    Fix assets en toon in grid

--- TheFellowShipMobileApp/TheFellowShipMobileApp/Views/HomeView.xaml.cs
+++ TheFellowShipMobileApp/TheFellowShipMobileApp/Views/HomeView.xaml.cs
@@ -39,23 +39,32 @@ namespace TheFellowShipMobileApp
             Console.WriteLine(surroundings.Count);
             Console.WriteLine(surroundings[0]);
 
-            for(int i = 0; i < 6; i++)
+            gameTiles.Children.Clear();
+            gameTiles.ColumnDefinitions.Clear();
+            gameTiles.RowDefinitions.Clear();
+
+            for (int i = 0; i < 6; i++)
             {
                 gameTiles.RowDefinitions.Add(new RowDefinition());
                 gameTiles.ColumnDefinitions.Add(new ColumnDefinition());
             }
             int counter = 0;
+            Console.WriteLine(surroundingsString);
             foreach (dynamic surrounding in surroundings)
             {
                 Location location = JsonConvert.DeserializeObject<Location>(Convert.ToString(surrounding));
                 Console.WriteLine(location.Type);
-                var label = new Label
+                var label = new Image
                 {
-                    Text = location.Type.ToString(),
+                    Source = $"{location.Type.ToString().Substring(0, 1).ToLower()}.png",
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center
                 };
-                gameTiles.Children.Add(label, counter++ % 6, counter / 6);
+                gameTiles.Children.Add(label, counter / 7, counter % 7);
+                Console.WriteLine(counter % 7);
+                Console.WriteLine(counter / 7);
+                Console.WriteLine(counter);
+                counter++;
             }
             
 
@@ -73,6 +82,33 @@ namespace TheFellowShipMobileApp
                     gameTiles.Children.Add(label, columnIndex, rowIndex);
                 }
             }*/
+        }
+
+        private void MoveButton_Clicked(object sender, EventArgs e)
+        {
+            int id;
+            switch ((sender as Button).Text)
+            {
+                case "Up":
+                    id = 1;
+                    break;
+                case "Down":
+                    id = 3;
+                    break;
+                case "Left":
+                    id = 4;
+                    break;
+                case "Right":
+                    id = 2;
+                    break;
+                default:
+                    id = 0;
+                    break;
+            }
+            
+            
+            App.Container.Resolve<IGameService>().MovePlayer(GameId, id);
+            CreateGameGrid();
         }
     }
 }
